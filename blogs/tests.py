1c29347538bf4d44f1f83b0cf78b49d60bfd0123
pythonPlant12 <pythonplant12@gmail.com>
2024-05-20 01:36:22 +0200

    - First test for testing environmental variables available access

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,1 +1,8 @@
 from django.test import TestCase
+import os
+
+
+class TryDjangoConfigTest(TestCase):
+    def test_config(self):
+        self.assertNotEqual(None,
+                         os.environ.get("DJANGO_SECRET_KEY"))
